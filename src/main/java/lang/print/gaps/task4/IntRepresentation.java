3335c3be57345a9ae4e87b0d7fbe6dd1beaf34e6
Daniel Oto <daniel.oto.mail@gmail.com>
2022-07-19 21:51:50 +0300

    module3/task4, integer types

--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,10 +2,10 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
-        int c = 1;
-        int h = 2;
-        int a = 3;
-        int r = 4;
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
         System.out.println(c);
         System.out.println(h);
         System.out.println(a);
